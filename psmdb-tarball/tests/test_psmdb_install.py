d20f96d21d38179d96b8c968af5102cd401cde75
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2021-09-13 14:25:56 +0300

    some base checks for binaries and running instances

--- psmdb-tarball/tests/test_psmdb_install.py
+++ psmdb-tarball/tests/test_psmdb_install.py
@@ -6,70 +6,39 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-#DEB_PACKAGES = ['percona-server-mongodb', 'percona-server-mongodb-server', 'percona-server-mongodb-mongos',
-#                'percona-server-mongodb-shell', 'percona-server-mongodb-tools', 'percona-server-mongodb-dbg']
-#RPM_PACKAGES = ['percona-server-mongodb', 'percona-server-mongodb-server', 'percona-server-mongodb-mongos',
-#                'percona-server-mongodb-shell', 'percona-server-mongodb-tools', 'percona-server-mongodb-debuginfo']
-#RPM_NEW_CENTOS_PACKAGES = ['percona-server-mongodb', 'percona-server-mongodb-mongos-debuginfo',
-#                           'percona-server-mongodb-server-debuginfo', 'percona-server-mongodb-shell-debuginfo',
-#                           'percona-server-mongodb-tools-debuginfo', 'percona-server-mongodb-debugsource']
-
 BINARIES = ['mongo', 'mongod', 'mongos', 'bsondump', 'mongoexport', 'mongobridge',
             'mongofiles', 'mongoimport', 'mongorestore', 'mongotop', 'mongostat']
 
 PSMDB_VER = os.environ.get("PSMDB_VERSION")
 
-
-def test_mongod_service(host):
-    mongod = host.service("mongod")
-    assert mongod.is_running
-
-
-#@pytest.mark.parametrize("package", DEB_PACKAGES)
-#def test_deb_packages(host, package):
-#    os = host.system_info.distribution
-#    if os.lower() in ["redhat", "centos", 'rhel']:
-#        pytest.skip("This test only for Debian based platforms")
-#    pkg = host.package(package)
-#    assert pkg.is_installed
-#    assert PSMDB_VER in pkg.version
-
-
-# TODO add check that minor version is correct
-#@pytest.mark.parametrize("package", RPM_PACKAGES)
-#def test_rpm_packages(host, package):
-#    os = host.system_info.distribution
-#    if os in ["debian", "ubuntu"]:
-#        pytest.skip("This test only for RHEL based platforms")
-#    if float(host.system_info.release) >= 8.0:
-#        pytest.skip("Only for centos7 tests")
-#    pkg = host.package(package)
-#    assert pkg.is_installed
-#    assert PSMDB_VER in pkg.version
-
-
-#@pytest.mark.parametrize("package", RPM_NEW_CENTOS_PACKAGES)
-#def test_rpm8_packages(host, package):
-#    os = host.system_info.distribution
-#    if os in ["debian", "ubuntu"]:
-#        pytest.skip("This test only for RHEL based platforms")
-#    if float(host.system_info.release) < 8.0:
-#        pytest.skip("Only for centos7 tests")
-#    pkg = host.package(package)
-#    assert pkg.is_installed
-#    assert PSMDB_VER in pkg.version
-
-
 @pytest.mark.parametrize("binary", BINARIES)
 def test_binary_version(host, binary):
-    result = host.run(f"{binary} --version")
+    mongod_process = host.process.filter(comm='mongod')
+    cur_dir = mongod_process[0].args.split(sep="mongod")[0]
+    print()
+    print('check binaries in',cur_dir,'directory')
+    result = host.run(f"{cur_dir}{binary} --version")
     assert PSMDB_VER in result.stdout, result.stdout
 
-def test_loaded_data(host):
-    cmd = "/package-testing/scripts/mongo_check.sh"
-    with host.sudo():
-        result = host.run(cmd)
-        print(result.stdout)
-        print(result.stderr)
-    assert result.rc == 0, result.stdout
+def test_running_version(host):
+    mongod_process = host.process.filter(comm='mongod')
+    print()
+    for i in mongod_process:
+        cur_dir = i.args.split(sep="mongod")[0]
+        cur_port = i.args.split(sep="--port ")[1].split(sep=' ')[0]
+        print('check mongod version running on port',cur_port)
+        result = host.run(f"{cur_dir}mongo --port {cur_port} --quiet --eval 'db.version()'")
+        assert PSMDB_VER in result.stdout, result.stdout
 
+def test_compatibility_version(host):
+    mongod_process = host.process.filter(comm='mongod')
+    COMP_VER = PSMDB_VER.split(sep='.')[0] + "." + PSMDB_VER.split(sep='.')[1]
+    print()
+    print('compatibility version has to be',COMP_VER)
+    for i in mongod_process:
+        cur_dir = i.args.split(sep="mongod")[0]
+        cur_port = i.args.split(sep="--port ")[1].split(sep=' ')[0]
+        print('check mongod compatibility version running on port',cur_port)
+        mongo_command = "db.adminCommand( { getParameter: 1, featureCompatibilityVersion: 1 } )"
+        result = host.run(f"{cur_dir}mongo --port {cur_port} --quiet --eval '{mongo_command}'")
+        assert COMP_VER in result.stdout, result.stdout
