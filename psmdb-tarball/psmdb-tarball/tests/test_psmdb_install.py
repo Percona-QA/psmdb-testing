c7df733377bc0249be629b6b2b900b8d7da9acdd
Oleksandr Havryliak <88387200+olexandr-havryliak@users.noreply.github.com>
2021-09-16 08:55:17 +0300

    Psmdb 919 tarball tests (#1)
    PSMDB-919 infrastructure for tarball tests

---
+++ psmdb-tarball/psmdb-tarball/tests/test_psmdb_install.py
@@ -0,0 +1,93 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+DEB_PACKAGES = ['percona-server-mongodb', 'percona-server-mongodb-server', 'percona-server-mongodb-mongos',
+                'percona-server-mongodb-shell', 'percona-server-mongodb-tools', 'percona-server-mongodb-dbg']
+RPM_PACKAGES = ['percona-server-mongodb', 'percona-server-mongodb-server', 'percona-server-mongodb-mongos',
+                'percona-server-mongodb-shell', 'percona-server-mongodb-tools', 'percona-server-mongodb-debuginfo']
+RPM_NEW_CENTOS_PACKAGES = ['percona-server-mongodb', 'percona-server-mongodb-mongos-debuginfo',
+                           'percona-server-mongodb-server-debuginfo', 'percona-server-mongodb-shell-debuginfo',
+                           'percona-server-mongodb-tools-debuginfo', 'percona-server-mongodb-debugsource']
+
+BINARIES = ['mongo', 'mongod', 'mongos', 'bsondump', 'mongoexport', 'mongobridge',
+            'mongofiles', 'mongoimport', 'mongorestore', 'mongotop', 'mongostat']
+
+PSMDB_VER = os.environ.get("PSMDB_VERSION")
+
+
+def test_functional(host):
+    cmd = "/package-testing/scripts/psmdb_test.sh" + ' ' + PSMDB_VER.split('.')[0] + '.' + PSMDB_VER.split('.')[1]
+    with host.sudo():
+        result = host.run(cmd)
+        print(result.stdout)
+        print(result.stderr)
+    assert result.rc == 0, result.stdout
+
+def test_enable_auth(host):
+    cmd = "/package-testing/scripts/psmdb_set_auth.sh"
+    with host.sudo():
+        result = host.run(cmd)
+        print(result.stdout)
+        print(result.stderr)
+    assert result.rc == 0, result.stdout
+
+def test_bats(host):
+    cmd = "/usr/local/bin/bats /package-testing/bats/mongo-init-scripts.bats"
+    with host.sudo():
+        result = host.run(cmd)
+        print(result.stdout)
+        print(result.stderr)
+    assert result.rc == 0, result.stdout
+
+
+def test_bats_with_numactl(host):
+    with host.sudo():
+        os = host.system_info.distribution
+        cmd = 'apt-get install numactl -y'
+        if os.lower() in ["redhat", "centos", 'rhel']:
+            cmd = 'yum install numactl -y'
+        result = host.run(cmd)
+        assert result.rc == 0, result.stdout
+        cmd = "/usr/local/bin/bats /package-testing/bats/mongo-init-scripts.bats"
+        result = host.run(cmd)
+        print(result.stdout)
+        print(result.stderr)
+    assert result.rc == 0, result.stdout
+
+def test_keyfile_encryption(host):
+    cmd = "/package-testing/scripts/psmdb_encryption/psmdb-encryption-test.sh keyfile"
+    with host.sudo():
+        result = host.run(cmd)
+        print(result.stdout)
+        print(result.stderr)
+    assert result.rc == 0, result.stdout
+
+def test_vault_encryption(host):
+    cmd = "/package-testing/scripts/psmdb_encryption/psmdb-encryption-test.sh vault"
+    with host.sudo():
+        result = host.run(cmd)
+        print(result.stdout)
+        print(result.stderr)
+    assert result.rc == 0, result.stdout
+
+def test_ldap_native(host):
+    cmd = "/package-testing/scripts/psmdb_ldap/psmdb_test_ldap.sh"
+    with host.sudo():
+        result = host.run(cmd)
+        print(result.stdout)
+        print(result.stderr)
+    assert result.rc == 0, result.stdout
+
+
+def test_load_data(host):
+    cmd = "/package-testing/scripts/mongo_insert.sh"
+    with host.sudo():
+        result = host.run(cmd)
+        print(result.stdout)
+        print(result.stderr)
+    assert result.rc == 0, result.stdout
---
+++ psmdb-tarball/tests/test_psmdb_install.py
@@ -0,0 +1,44 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+BINARIES = ['mongo', 'mongod', 'mongos', 'bsondump', 'mongoexport', 'mongobridge',
+            'mongofiles', 'mongoimport', 'mongorestore', 'mongotop', 'mongostat']
+
+PSMDB_VER = os.environ.get("PSMDB_VERSION")
+
+@pytest.mark.parametrize("binary", BINARIES)
+def test_binary_version(host, binary):
+    mongod_process = host.process.filter(comm='mongod')
+    cur_dir = mongod_process[0].args.split(sep="mongod")[0]
+    print()
+    print('check binaries in',cur_dir,'directory')
+    result = host.run(f"{cur_dir}{binary} --version")
+    assert PSMDB_VER in result.stdout, result.stdout
+
+def test_running_version(host):
+    mongod_process = host.process.filter(comm='mongod')
+    print()
+    for i in mongod_process:
+        cur_dir = i.args.split(sep="mongod")[0]
+        cur_port = i.args.split(sep="--port ")[1].split(sep=' ')[0]
+        print('check mongod version running on port',cur_port)
+        result = host.run(f"{cur_dir}mongo --port {cur_port} --quiet --eval 'db.version()'")
+        assert PSMDB_VER in result.stdout, result.stdout
+
+def test_compatibility_version(host):
+    mongod_process = host.process.filter(comm='mongod')
+    COMP_VER = PSMDB_VER.split(sep='.')[0] + "." + PSMDB_VER.split(sep='.')[1]
+    print()
+    print('compatibility version has to be',COMP_VER)
+    for i in mongod_process:
+        cur_dir = i.args.split(sep="mongod")[0]
+        cur_port = i.args.split(sep="--port ")[1].split(sep=' ')[0]
+        print('check mongod compatibility version running on port',cur_port)
+        mongo_command = "db.adminCommand( { getParameter: 1, featureCompatibilityVersion: 1 } )"
+        result = host.run(f"{cur_dir}mongo --port {cur_port} --quiet --eval '{mongo_command}'")
+        assert COMP_VER in result.stdout, result.stdout
