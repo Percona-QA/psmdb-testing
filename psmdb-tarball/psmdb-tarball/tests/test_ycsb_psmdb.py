94371db9a6432fc2284383883402bfbbc6d73aa4
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2021-09-27 16:14:02 +0300

    test testuite properties

--- psmdb-tarball/psmdb-tarball/tests/test_ycsb_psmdb.py
+++ psmdb-tarball/psmdb-tarball/tests/test_ycsb_psmdb.py
@@ -6,7 +6,17 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_ycsb_load(host,record_property):
+#def test_ycsb_load(host,record_property):
+#    cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb load mongodb -s -P workloads/workloadb  -p mongodb.auth="false" | tail -n +2'
+#    with host.sudo():
+#        result = host.run(cmd)
+#        print(result.stdout)
+#    for line in result.stdout.splitlines():
+#        name, descr, timing = line.split(',')
+#        fullname = name + ' ' + descr
+#        record_property(fullname, timing)
+
+def test_ycsb_load(host,record_testsuite_property):
     cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb load mongodb -s -P workloads/workloadb  -p mongodb.auth="false" | tail -n +2'
     with host.sudo():
         result = host.run(cmd)
@@ -14,7 +24,8 @@ def test_ycsb_load(host,record_property):
     for line in result.stdout.splitlines():
         name, descr, timing = line.split(',')
         fullname = name + ' ' + descr
-        record_property(fullname, timing)
+        record_testsuite_property(fullname, timing)
+
 
 def test_ycsb_run(host,record_property):
     cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb run mongodb -s -P workloads/workloadb  -p mongodb.auth="false" | tail -n +2'
