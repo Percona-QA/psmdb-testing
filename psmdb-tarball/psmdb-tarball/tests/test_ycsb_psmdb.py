a0b52f9e68159ccae961e00cd837b0aea249d8d8
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2021-09-27 17:51:10 +0300

    check stdou capture

--- psmdb-tarball/psmdb-tarball/tests/test_ycsb_psmdb.py
+++ psmdb-tarball/psmdb-tarball/tests/test_ycsb_psmdb.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import pytest
 
 import testinfra.utils.ansible_runner
@@ -6,34 +7,17 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-#def test_ycsb_load(host,record_property):
-#    cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb load mongodb -s -P workloads/workloadb  -p mongodb.auth="false" | tail -n +2'
-#    with host.sudo():
-#        result = host.run(cmd)
-#        print(result.stdout)
-#    for line in result.stdout.splitlines():
-#        name, descr, timing = line.split(',')
-#        fullname = name + ' ' + descr
-#        record_property(fullname, timing)
-
-def test_ycsb_load(host,record_testsuite_property):
-    cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb load mongodb -s -P workloads/workloadb  -p mongodb.auth="false" | tail -n +2'
+def test_ycsb_load(host):
+    cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb load mongodb -s -P workloads/workloadb  -p mongodb.auth="false"'
     with host.sudo():
         result = host.run(cmd)
-        print(result.stdout)
     for line in result.stdout.splitlines():
-        name, descr, timing = line.split(',')
-        fullname = name + ' ' + descr
-        record_testsuite_property(fullname, timing)
+        print(line)
 
-
-def test_ycsb_run(host,record_property):
-    cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb run mongodb -s -P workloads/workloadb  -p mongodb.auth="false" | tail -n +2'
+def test_ycsb_run(host,capsys):
+    cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb run mongodb -s -P workloads/workloadb  -p mongodb.auth="false"'
     with host.sudo():
         result = host.run(cmd)
-        print(result.stdout) 
     for line in result.stdout.splitlines():
-        name, descr, timing = line.split(',')
-        fullname = name + ' ' + descr
-        record_property(fullname, timing)
-  
+        print(line)
+
