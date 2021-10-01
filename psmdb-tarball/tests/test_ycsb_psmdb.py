5405d4f6a96d2e8b98fe829abdfbbeff98f71de4
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2021-10-01 08:34:47 +0300

    moved ycsb load to ansible, different uri for different setups

--- psmdb-tarball/tests/test_ycsb_psmdb.py
+++ psmdb-tarball/tests/test_ycsb_psmdb.py
@@ -7,15 +7,13 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_ycsb_load(host):
-    cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb load mongodb -s -P workloads/workloadb  -p mongodb.auth="false"'
-    with host.sudo():
-        result = host.run(cmd)
-    for line in result.stdout.splitlines():
-        print(line)
+layout = os.environ.get("LAYOUT_TYPE")
 
-def test_ycsb_run(host,capsys):
-    cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb run mongodb -s -P workloads/workloadb  -p mongodb.auth="false"'
+def test_ycsb_run(host):
+    if layout == "replicaset":
+        cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb run mongodb -s -P workloads/workloadb -p recordcount=100000 -p operationcount=100000 -p mongodb.url="mongodb://localhost:27017/ycsb?replicaSet=rs0&w=1" -p mongodb.auth="false"'
+    else:
+        cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb run mongodb -s -P workloads/workloadb -p recordcount=100000 -p operationcount=100000 -p mongodb.auth="false"'
     with host.sudo():
         result = host.run(cmd)
     for line in result.stdout.splitlines():
