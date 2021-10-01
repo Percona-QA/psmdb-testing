9c485e499cc2772a729925643bdfd13c8c9a97b1
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2021-10-01 10:35:52 +0300

    ycsb tuning

--- psmdb-tarball/tests/test_ycsb_psmdb.py
+++ psmdb-tarball/tests/test_ycsb_psmdb.py
@@ -11,9 +11,9 @@ layout = os.environ.get("LAYOUT_TYPE")
 
 def test_ycsb_run(host):
     if layout == "replicaset":
-        cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb run mongodb -s -P workloads/workloadb -p recordcount=100000 -p operationcount=100000 -p mongodb.url="mongodb://localhost:27017/ycsb?replicaSet=rs0&w=1" -p mongodb.auth="false"'
+        cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb run mongodb -s -P workloads/workloadb -p recordcount=10000 -p operationcount=10000 -p mongodb.url="mongodb://localhost:27017/ycsb?replicaSet=rs0&w=1" -p mongodb.auth="false"'
     else:
-        cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb run mongodb -s -P workloads/workloadb -p recordcount=100000 -p operationcount=100000 -p mongodb.auth="false"'
+        cmd = 'cd /workdir/ycsb-mongodb-binding && ./bin/ycsb run mongodb -s -P workloads/workloadb -p recordcount=10000 -p operationcount=10000 -p mongodb.auth="false"'
     with host.sudo():
         result = host.run(cmd)
     for line in result.stdout.splitlines():
