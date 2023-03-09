fa2883688258aec53fd35d891e8007e6eaa26ea6
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2023-03-09 16:40:42 +0200

    PBM-754 added azurite as azure-like blob-storage

---
+++ pbm-functional/pytest/test_azurite.py
@@ -0,0 +1,92 @@
+import pytest
+import pymongo
+import bson
+import testinfra
+import time
+import os
+import docker
+import threading
+
+from datetime import datetime
+from cluster import Cluster
+
+documents = [{"a": 1}, {"b": 2}, {"c": 3}, {"d": 4}]
+
+@pytest.fixture(scope="package")
+def docker_client():
+    return docker.from_env()
+
+@pytest.fixture(scope="package")
+def config():
+    return {"mongos": "mongos",
+            "configserver":
+            {"_id": "rscfg", "members": [{"host": "rscfg01"}, {
+                "host": "rscfg02"}, {"host": "rscfg03"}]},
+            "shards": [
+                {"_id": "rs1", "members": [{"host": "rs101"}, {
+                    "host": "rs102"}, {"host": "rs103"}]},
+                {"_id": "rs2", "members": [{"host": "rs201"}, {
+                    "host": "rs202"}, {"host": "rs203"}]}
+            ]}
+
+@pytest.fixture(scope="package")
+def cluster(config):
+    return Cluster(config)
+
+@pytest.fixture(scope="function")
+def start_cluster(cluster, request):
+    try:
+        cluster.destroy()
+        cluster.create()
+        cluster.setup_pbm()
+        azure_account = "devstoreaccount1"
+        azure_key = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
+        result = cluster.exec_pbm_cli("config --set storage.type=azure --set storage.azure.account=" + azure_account +
+                                      " --set storage.azure.container=test-container --set storage.azure.credentials.key=" + azure_key)
+        client = pymongo.MongoClient(cluster.connection)
+        client.admin.command("enableSharding", "test")
+        client.admin.command(
+            "shardCollection", "test.test", key={"_id": "hashed"})
+        yield True
+    finally:
+        if request.config.getoption("--verbose"):
+            cluster.get_logs()
+        cluster.destroy()
+
+@pytest.mark.timeout(300, func_only=True)
+def test_logical(start_cluster, cluster):
+    cluster.check_pbm_status()
+    pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
+    backup = cluster.make_backup("logical")
+    result = pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
+    assert int(result.deleted_count) == len(documents)
+    cluster.make_restore(backup, check_pbm_status=True)
+    assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == len(documents)
+    assert pymongo.MongoClient(cluster.connection)["test"].command("collstats", "test").get("sharded", False)
+    print("\nFinished successfully\n")
+
+@pytest.mark.timeout(300, func_only=True)
+def test_physical(start_cluster, cluster):
+    cluster.check_pbm_status()
+    pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
+    backup = cluster.make_backup("physical")
+    result = pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
+    assert int(result.deleted_count) == len(documents)
+    cluster.make_restore(backup, restart_cluster=True,make_resync=True, check_pbm_status=True)
+    assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == len(documents)
+    assert pymongo.MongoClient(cluster.connection)["test"].command("collstats", "test").get("sharded", False)
+    print("\nFinished successfully\n")
+
+@pytest.mark.timeout(300, func_only=True)
+def test_incremental(start_cluster, cluster):
+    cluster.check_pbm_status()
+    cluster.make_backup("incremental --base")
+    pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
+    backup = cluster.make_backup("incremental")
+    result = pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
+    assert int(result.deleted_count) == len(documents)
+    cluster.make_restore(backup, restart_cluster=True,make_resync=True, check_pbm_status=True)
+    assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == len(documents)
+    assert pymongo.MongoClient(cluster.connection)["test"].command("collstats", "test").get("sharded", False)
+    print("\nFinished successfully\n")
+
