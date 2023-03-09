549233a98e031a27069c405b497dcdf192739ea2
Oleksandr Havryliak <88387200+olexandr-havryliak@users.noreply.github.com>
2023-03-09 13:42:34 +0200

    PBM-754 Added upgrade tests, other improvements (#21)

--- pbm-functional/pytest/test_upgrade.py
+++ pbm-functional/pytest/test_upgrade.py
@@ -17,23 +17,48 @@ def docker_client():
 
 @pytest.fixture(scope="package")
 def config():
-    return { "_id": "rs1", "members": [{"host":"rs101"},{"host": "rs102"},{"host": "rs103" }]}
+    return { "mongos": "mongos",
+             "configserver":
+                            {"_id": "rscfg", "members": [{"host":"rscfg01"},{"host": "rscfg02"},{"host": "rscfg03" }]},
+             "shards":[
+                            {"_id": "rs1", "members": [{"host":"rs101"},{"host": "rs102"},{"host": "rs103" }]},
+                            {"_id": "rs2", "members": [{"host":"rs201"},{"host": "rs202"},{"host": "rs203" }]}
+                      ]}
 
 @pytest.fixture(scope="package")
 def cluster(config):
-    return Cluster(config, mongod_extra_args="--enableEncryption --encryptionKeyFile=/etc/mongodb-keyfile")
+    return Cluster(config)
 
 @pytest.fixture(scope="function")
 def start_cluster(cluster,request):
     try:
         cluster.destroy()
         cluster.create()
+        cluster.downgrade()
         cluster.setup_pbm()
+        client=pymongo.MongoClient(cluster.connection)
+        client.admin.command("enableSharding", "test")
+        client.admin.command("shardCollection", "test.test", key={"_id": "hashed"})
         yield True
+
     finally:
         if request.config.getoption("--verbose"):
             cluster.get_logs()
         cluster.destroy()
+
+@pytest.mark.timeout(300,func_only=True)
+def test_logical(start_cluster,cluster):
+    cluster.check_pbm_status()
+    pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
+    backup=cluster.make_backup("logical")
+    result=pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
+    assert int(result.deleted_count) == len(documents)
+    cluster.upgrade()
+    cluster.check_pbm_status()
+    cluster.make_restore(backup,check_pbm_status=True)
+    assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == len(documents)
+    assert pymongo.MongoClient(cluster.connection)["test"].command("collstats", "test").get("sharded", False)
+    print("\nFinished successfully\n")
 
 @pytest.mark.timeout(300,func_only=True)
 def test_physical(start_cluster,cluster):
@@ -42,8 +67,11 @@ def test_physical(start_cluster,cluster):
     backup=cluster.make_backup("physical")
     result=pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
     assert int(result.deleted_count) == len(documents)
+    cluster.upgrade()
+    cluster.check_pbm_status()
     cluster.make_restore(backup,restart_cluster=True, make_resync=True, check_pbm_status=True)
     assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == len(documents)
+    assert pymongo.MongoClient(cluster.connection)["test"].command("collstats", "test").get("sharded", False)
     print("\nFinished successfully\n")
 
 @pytest.mark.timeout(300,func_only=True)
@@ -51,10 +79,13 @@ def test_incremental(start_cluster,cluster):
     cluster.check_pbm_status()
     cluster.make_backup("incremental --base")
     pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
-    backup=cluster.make_backup("incremental")
+    backup = cluster.make_backup("incremental")
     result=pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
     assert int(result.deleted_count) == len(documents)
+    cluster.upgrade()
+    cluster.check_pbm_status()
     cluster.make_restore(backup,restart_cluster=True, make_resync=True, check_pbm_status=True)
     assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == len(documents)
+    assert pymongo.MongoClient(cluster.connection)["test"].command("collstats", "test").get("sharded", False)
     print("\nFinished successfully\n")
 
