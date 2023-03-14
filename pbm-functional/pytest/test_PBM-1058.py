4ddeba5dc9741d4065edb777333af54e3f7b4cff
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2023-03-14 20:40:11 +0200

    PBM-1058 datadir with tailing slash

---
+++ pbm-functional/pytest/test_PBM-1058.py
@@ -0,0 +1,53 @@
+import pytest
+import pymongo
+import bson
+import testinfra
+import time
+import os
+import docker
+
+from datetime import datetime
+from cluster import Cluster
+
+documents=[{"a": 1}, {"b": 2}, {"c": 3}, {"d": 4}]
+
+@pytest.fixture(scope="package")
+def docker_client():
+    return docker.from_env()
+
+@pytest.fixture(scope="package")
+def config():
+    return { "mongos": "mongos",
+             "configserver":
+                            {"_id": "rscfg", "members": [{"host": "rscfg01"}, {"host": "rscfg02"}, {"host": "rscfg03"}]},
+             "shards":[
+                            {"_id": "rs1", "members": [{"host":"rs101"}]}
+                      ]}
+
+@pytest.fixture(scope="package")
+def cluster(config):
+    return Cluster(config, mongod_datadir="/var/lib/mongo/")
+
+@pytest.fixture(scope="function")
+def start_cluster(cluster,request):
+    try:
+        cluster.destroy()
+        cluster.create()
+        cluster.setup_pbm()
+        yield True
+    finally:
+        if request.config.getoption("--verbose"):
+            cluster.get_logs()
+        cluster.destroy()
+
+@pytest.mark.timeout(300,func_only=True)
+def test_physical(start_cluster,cluster):
+    cluster.check_pbm_status()
+    pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
+    backup=cluster.make_backup("physical")
+    result=pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
+    assert int(result.deleted_count) == len(documents)
+    cluster.make_restore(backup,restart_cluster=True, make_resync=True, check_pbm_status=True)
+    assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == len(documents)
+    print("\nFinished successfully\n")
+
