3b0d5555e33f51cd12e99d13bf62577da41566d8
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2023-03-20 19:06:19 +0200

    PBM-754 added script for manual cluster setup

---
+++ pbm-functional/pytest/example_cluster.py
@@ -0,0 +1,27 @@
+import signal
+from cluster import Cluster
+
+#docker-compose run -ti --rm test python3 -i example_cluster.py
+
+config = { "mongos": "mongos",
+           "configserver":
+                        {"_id": "rscfg", "members": [{"host": "rscfg01"}, {"host": "rscfg02"}, {"host": "rscfg03"}]},
+           "shards":[
+                        {"_id": "rs1", "members": [{"host": "rs101"}, {"host": "rs102"}, {"host": "rs103"}]},
+                        {"_id": "rs2", "members": [{"host": "rs201"}, {"host": "rs202"}, {"host": "rs203"}]}
+                    ]}
+
+cluster = Cluster(config)
+
+def handler(signum,frame):
+    cluster.destroy()
+    exit(0)
+
+cluster.destroy()
+cluster.create()
+cluster.setup_pbm()
+
+signal.signal(signal.SIGINT,handler)
+print("\nCluster is prepared and ready to use")
+print("\nPress CTRL-C to destroy and exit")
+
