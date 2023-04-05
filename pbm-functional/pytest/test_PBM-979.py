ef7170e52b9da15889c937f1dc1f5475b041137b
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2023-04-05 09:13:39 +0300

    PBM-979 merged psa and psh tests into single suite

--- pbm-functional/pytest/test_PBM-979.py
+++ pbm-functional/pytest/test_PBM-979.py
@@ -32,7 +32,7 @@ def config(mongod_version):
             {"host": "rs102", "priority": 2 },
             {"host": "rs103", "hidden": True, "priority": 0, "votes": 0},
             {"host": "rs104", "slaveDelay": 1, "priority": 0, "votes": 0, "buildIndexes": False },
-            {"host": "rs105", "priority": 3}
+            {"host": "rs105", "arbiterOnly": True }
         ]}
     else:
         return { "_id": "rs1", "members": [
@@ -40,7 +40,7 @@ def config(mongod_version):
             {"host": "rs102", "priority": 2 },
             {"host": "rs103", "hidden": True, "priority": 0, "votes": 0},
             {"host": "rs104", "secondaryDelaySecs": 1, "priority": 0, "votes": 0, "buildIndexes": False },
-            {"host": "rs105", "priority": 3}
+            {"host": "rs105", "arbiterOnly": True }
         ]}
 
 
@@ -53,6 +53,7 @@ def start_cluster(cluster,request):
     try:
         cluster.destroy()
         cluster.create()
+        time.sleep(5) # wait for delayed node
         cluster.setup_pbm()
         yield True
     finally:
@@ -61,16 +62,37 @@ def start_cluster(cluster,request):
         cluster.destroy()
 
 @pytest.mark.timeout(300,func_only=True)
+def test_logical(start_cluster,cluster):
+    time.sleep(5) # wait for delayed node
+    cluster.check_pbm_status()
+    pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
+    backup=cluster.make_backup("logical")
+    #check if the backup was taken from the hidden node
+    logs=cluster.exec_pbm_cli("logs -n rs1/rs103:27017 -e backup -o json").stdout
+    assert backup in logs
+    Cluster.log("Logs from hidden node:\n" + logs)
+    result=pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
+    assert int(result.deleted_count) == len(documents)
+    cluster.make_restore(backup)
+    time.sleep(5) # wait for delayed node
+    cluster.check_pbm_status()
+    assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == len(documents)
+    Cluster.log("Finished successfully")
+
+@pytest.mark.timeout(300,func_only=True)
 def test_physical(start_cluster,cluster):
     time.sleep(5) # wait for delayed node
     cluster.check_pbm_status()
     pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
     backup=cluster.make_backup("physical")
+    #check if the backup was taken from the hidden node
+    logs=cluster.exec_pbm_cli("logs -n rs1/rs103:27017 -e backup -o json").stdout
+    assert backup in logs
+    Cluster.log("Logs from hidden node:\n" + logs)
     result=pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
     assert int(result.deleted_count) == len(documents)
     cluster.make_restore(backup,restart_cluster=True)
     time.sleep(5) # wait for delayed node
-    cluster.check_pbm_status()
     assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == len(documents)
     Cluster.log("Cluster config: \n" + str(cluster.config))
     rs_config = pymongo.MongoClient(cluster.connection).admin.command('replSetGetConfig')['config']
@@ -90,11 +112,15 @@ def test_physical(start_cluster,cluster):
 def test_incremental(start_cluster,cluster):
     time.sleep(5)
     cluster.check_pbm_status()
-    cluster.make_backup("incremental --base")
+    init_backup=cluster.make_backup("incremental --base")
     pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
     backup=cluster.make_backup("incremental")
     result=pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
     assert int(result.deleted_count) == len(documents)
+    logs=cluster.exec_pbm_cli("logs -n rs1/rs103:27017 -e backup -o json").stdout
+    Cluster.log("Logs from hidden node:\n" + logs)
+    assert init_backup in logs
+    assert backup in logs
     cluster.make_restore(backup,restart_cluster=True)
     time.sleep(5)
     cluster.check_pbm_status()
