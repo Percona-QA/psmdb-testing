e2c4b0c9695c3bc3c870b1d690e738d0f19d8a15
Oleksandr Havryliak <88387200+olexandr-havryliak@users.noreply.github.com>
2023-04-12 18:48:50 +0300

    PSMDB tarballs testing speedup (#39)

--- psmdb-tarball/psmdb-tarball/tests/test_psmdb_tarball.py
+++ psmdb-tarball/psmdb-tarball/tests/test_psmdb_tarball.py
@@ -7,20 +7,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-SUITES = ['core']
 JSTESTS = ['test_kerberos_simple.js','test_ldap_simple.js']
 
 @pytest.mark.parametrize("jstest", JSTESTS)
 def test_jstests(host, jstest):
     cmd = "cd /percona-server-mongodb && python3 buildscripts/resmoke.py run /psmdb-testing/jstests/"  + jstest 
-    with host.sudo():
-        result = host.run(cmd)
-        print(result.stderr)
-    assert result.rc == 0, result.stdout
-
-@pytest.mark.parametrize("suite", SUITES)
-def test_suites(host, suite):
-    cmd = "cd /percona-server-mongodb && python3 buildscripts/resmoke.py run --suite" + ' '  + suite 
     with host.sudo():
         result = host.run(cmd)
         print(result.stderr)
