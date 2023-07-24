56bddec07186674d05f8c7e58983c287fbdb0c12
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2023-07-24 16:20:55 +0300

    PBM-1155 test restore when incremental backup was taken with retries

--- pbm-functional/pytest/test_PBM-1155.py
+++ pbm-functional/pytest/test_PBM-1155.py
@@ -39,7 +39,10 @@ def start_cluster(cluster,request):
             document = {
                 'data': random.randbytes(1024)
             }
-            pymongo.MongoClient(cluster.connection)[db][collection].insert_one(document)
+            try:
+                pymongo.MongoClient(cluster.connection)[db][collection].insert_one(document)
+            except BaseException:
+                break
         Cluster.log("Stopping background insert to " + collection)
     try:
         cluster.destroy()
@@ -47,6 +50,7 @@ def start_cluster(cluster,request):
         cluster.setup_pbm()
         client=pymongo.MongoClient(cluster.connection)
         client.admin.command("enableSharding", "test")
+        client.admin.command("shardCollection", "test.test", key={"_id": "hashed"})
         client.admin.command("shardCollection", "test.test1", key={"_id": "hashed"})
         client.admin.command("shardCollection", "test.test2", key={"_id": "hashed"})
         for node in cluster.pbm_hosts:
@@ -92,8 +96,14 @@ def test_incremental(start_cluster,cluster):
     cluster.make_backup("incremental --base")
     for i in range(20):
         time.sleep(1)
-        cluster.make_backup("incremental")
+        pymongo.MongoClient(cluster.connection)['test']['test'].insert_one({'data': random.randbytes(1024)})
+        backup=cluster.make_backup("incremental")
 
+    cluster.make_restore(backup,restart_cluster=True, check_pbm_status=True)
+    assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == 20
+    assert pymongo.MongoClient(cluster.connection)["test"].command("collstats", "test").get("sharded", False)
+    assert pymongo.MongoClient(cluster.connection)["test"].command("collstats", "test1").get("sharded", False)
+    assert pymongo.MongoClient(cluster.connection)["test"].command("collstats", "test2").get("sharded", False)
     Cluster.log("Finished successfully")
 
 @pytest.mark.testcase(test_case_key="T247", test_step_key=1)
