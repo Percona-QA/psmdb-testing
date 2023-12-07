da18df67a5b47ac5948c4265c16466e706bb1a63
Oleksandr Havryliak <88387200+olexandr-havryliak@users.noreply.github.com>
2023-12-07 16:13:32 +0200

    PSMDB mongosh version (#105)
    * PSMDB added mongosh version parameter
    
    * PDMDB added mongosh version parameter
    
    * PSMDB tests junit reports
    
    * Single place to store mongosh version
    
    * Removed newline for mongosh version check

--- pdmdb/tests/test_psmdb_install.py
+++ pdmdb/tests/test_psmdb_install.py
@@ -1,6 +1,7 @@
 import os
 import pytest
-
+import requests
+from packaging import version
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -21,7 +22,14 @@ BINARIES = ['mongod', 'mongos', 'bsondump', 'mongoexport', 'mongobridge',
             'mongofiles', 'mongoimport', 'mongorestore', 'mongotop', 'mongostat']
 
 PSMDB_VER = os.environ.get("PDMDB_VERSION").lstrip("pdmdb-")
+TESTING_BRANCH = os.environ.get("TESTING_BRANCH")
 
+def get_mongosh_ver():
+    url = "https://raw.githubusercontent.com/Percona-QA/psmdb-testing/" + TESTING_BRANCH + "/MONGOSH_VERSION"
+    r = requests.get(url)
+    return r.text.rstrip()
+
+MONGOSH_VER = get_mongosh_ver()
 
 def test_mongod_service(host):
     mongod = host.service("mongod")
@@ -77,6 +85,13 @@ def test_rpm9_packages(host, package):
 def test_binary_version(host, binary):
     result = host.run(f"{binary} --version")
     assert PSMDB_VER in result.stdout, result.stdout
+
+def test_cli_version(host):
+    result = host.check_output("mongo --version")
+    if version.parse(PSMDB_VER) > version.parse("6.0.0"):
+        assert MONGOSH_VER in result
+    else:
+        assert PSMDB_VER in result
 
 def test_telemetry(host):
     file_path = "/usr/local/percona/telemetry_uuid"
--- psmdb/tests/test_psmdb_install.py
+++ psmdb/tests/test_psmdb_install.py
@@ -1,69 +1,42 @@
 import os
 import pytest
-
+import requests
+from packaging import version
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-#DEB_PACKAGES = ['percona-server-mongodb', 'percona-server-mongodb-server', 'percona-server-mongodb-mongos',
-#                'percona-server-mongodb-shell', 'percona-server-mongodb-tools', 'percona-server-mongodb-dbg']
-#RPM_PACKAGES = ['percona-server-mongodb', 'percona-server-mongodb-server', 'percona-server-mongodb-mongos',
-#                'percona-server-mongodb-shell', 'percona-server-mongodb-tools', 'percona-server-mongodb-debuginfo']
-#RPM_NEW_CENTOS_PACKAGES = ['percona-server-mongodb', 'percona-server-mongodb-mongos-debuginfo',
-#                           'percona-server-mongodb-server-debuginfo', 'percona-server-mongodb-shell-debuginfo',
-#                           'percona-server-mongodb-tools-debuginfo', 'percona-server-mongodb-debugsource']
 
 BINARIES = ['mongod', 'mongos', 'bsondump', 'mongoexport', 'mongobridge',
             'mongofiles', 'mongoimport', 'mongorestore', 'mongotop', 'mongostat']
 
 PSMDB_VER = os.environ.get("PSMDB_VERSION")
+TESTING_BRANCH = os.environ.get("TESTING_BRANCH")
 
+def get_mongosh_ver():
+    url = "https://raw.githubusercontent.com/Percona-QA/psmdb-testing/" + TESTING_BRANCH + "/MONGOSH_VERSION"
+    r = requests.get(url)
+    return r.text.rstrip()
+
+MONGOSH_VER = get_mongosh_ver()
 
 def test_mongod_service(host):
     mongod = host.service("mongod")
     assert mongod.is_running
 
 
-#@pytest.mark.parametrize("package", DEB_PACKAGES)
-#def test_deb_packages(host, package):
-#    os = host.system_info.distribution
-#    if os.lower() in ["redhat", "centos", 'rhel']:
-#        pytest.skip("This test only for Debian based platforms")
-#    pkg = host.package(package)
-#    assert pkg.is_installed
-#    assert PSMDB_VER in pkg.version
-
-
-# TODO add check that minor version is correct
-#@pytest.mark.parametrize("package", RPM_PACKAGES)
-#def test_rpm_packages(host, package):
-#    os = host.system_info.distribution
-#    if os in ["debian", "ubuntu"]:
-#        pytest.skip("This test only for RHEL based platforms")
-#    if float(host.system_info.release) >= 8.0:
-#        pytest.skip("Only for centos7 tests")
-#    pkg = host.package(package)
-#    assert pkg.is_installed
-#    assert PSMDB_VER in pkg.version
-
-
-#@pytest.mark.parametrize("package", RPM_NEW_CENTOS_PACKAGES)
-#def test_rpm8_packages(host, package):
-#    os = host.system_info.distribution
-#    if os in ["debian", "ubuntu"]:
-#        pytest.skip("This test only for RHEL based platforms")
-#    if float(host.system_info.release) < 8.0:
-#        pytest.skip("Only for centos7 tests")
-#    pkg = host.package(package)
-#    assert pkg.is_installed
-#    assert PSMDB_VER in pkg.version
-
-
 @pytest.mark.parametrize("binary", BINARIES)
 def test_binary_version(host, binary):
     result = host.run(f"{binary} --version")
     assert PSMDB_VER in result.stdout, result.stdout
+
+def test_cli_version(host):
+    result = host.check_output("mongo --version")
+    if version.parse(PSMDB_VER) > version.parse("6.0.0"):
+        assert MONGOSH_VER in result
+    else:
+        assert PSMDB_VER in result
 
 def test_loaded_data(host):
     cmd = "/package-testing/scripts/mongo_check.sh"
