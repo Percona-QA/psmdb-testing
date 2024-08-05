a2bde617edc9b78ff5c5a7df4ee201e6aed35e6d
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2024-08-05 11:10:30 +0300

    PBM remove crutches for azurite

--- pbm-functional/pytest/test_azurite.py
+++ pbm-functional/pytest/test_azurite.py
@@ -41,7 +41,9 @@ def start_cluster(cluster, request):
         cluster.setup_pbm()
         azure_account = "devstoreaccount1"
         azure_key = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
-        result = cluster.exec_pbm_cli("config --set storage.type=azure --set storage.azure.account=" + azure_account +
+        azure_endpoint = "http://azurite:10000/" + azure_account
+        result = cluster.exec_pbm_cli("config --set storage.type=azure --set storage.azure.endpointUrl=" + azure_endpoint +
+                                      " --set storage.azure.account=" + azure_account +
                                       " --set storage.azure.container=test-container --set storage.azure.credentials.key=" + azure_key +
                                       " --out json")
         Cluster.log("Setup PBM with azurite storage:\n" + result.stdout)
