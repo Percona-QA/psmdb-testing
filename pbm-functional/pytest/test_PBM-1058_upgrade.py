9755d9a58c816b9ce1b2661745aa710a254d5928
Oleksandr Havryliak <oleksandr.havryliak@percona.com>
2024-08-08 12:31:59 +0300

    PBM-1058 disable the upgrade test

--- pbm-functional/pytest/test_PBM-1058_upgrade.py
+++ pbm-functional/pytest/test_PBM-1058_upgrade.py
@@ -49,7 +49,7 @@ def start_cluster(cluster,request):
         cluster.destroy(cleanup_backups=True)
 
 @pytest.mark.timeout(300,func_only=True)
-def test_physical_PBM_T235(start_cluster,cluster):
+def test_disabled_PBM_T235(start_cluster,cluster):
     cluster.check_pbm_status()
     pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
     backup=cluster.make_backup("physical")
