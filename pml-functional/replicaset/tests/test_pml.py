7baf5eced2da3e3bf2e6c1886fd4c4ef157e2212
Keith Quinn <keith.quinn@percona.com>
2025-04-08 09:19:14 +0100

    PML-121 Test changes

--- pml-functional/replicaset/tests/test_pml.py
+++ pml-functional/replicaset/tests/test_pml.py
@@ -212,10 +212,10 @@ def setup_pitr(node,port):
     print(store_out)
 
 def test_3_prepare_data():
-    print(source)
-    # load_data(source,"27017",SIZE)
-    # count = check_count_data(source,"27017")
-    # assert int(count) == SIZE
+    # print(source)
+    load_data(source,"27017",SIZE)
+    count = check_count_data(source,"27017")
+    assert int(count) == SIZE
 
 # def test_1_print():
 #     print("\nThe infrastructure is ready, waiting " + str(TIMEOUT) + " seconds")
