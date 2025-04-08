06a59a5562eac525df4d0e501ba5b2ff107e8465
Keith Quinn <keith.quinn@percona.com>
2025-04-08 16:35:21 +0100

    PML-121 Testing distribution changes

--- pml-functional/replicaset/tests/test_pml.py
+++ pml-functional/replicaset/tests/test_pml.py
@@ -39,8 +39,11 @@ def distribute_create_config(dataSize, collections):
     print("KEITH TEST: " + str(distribution_chunks))
 
     for x in range(collections):
-        distribution = dataSize / 100 * distribution_chunks[x]
-        print("KEITH TEST: " + str(int(distribution)))
+        test1 = dataSize / 100
+        print("1: " + str(test1))
+        # print("1: " + str(test1))
+        # distribution = dataSize / 100 * distribution_chunks[x]
+        print("KEITH TEST SPLIT: " + str(int(distribution)))
         collectionName = f"collection{x}"
         string2 = {'database': 'test','collection': collectionName,'count': documentCount,'content': {'binary': {'type': 'binary','minLength': 10485760, 'maxLength': 10485760}}}
         string.append(string2)
