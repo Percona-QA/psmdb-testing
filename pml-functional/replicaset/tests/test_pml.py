bb8ee9fef92baca31137c0b209112007500ecf26
Keith Quinn <keith.quinn@percona.com>
2025-04-08 15:26:29 +0100

    PML-121 Testing changes

--- pml-functional/replicaset/tests/test_pml.py
+++ pml-functional/replicaset/tests/test_pml.py
@@ -14,39 +14,23 @@ destination = testinfra.utils.ansible_runner.AnsibleRunner(
 pml = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_host('jenkins-pml-mongolink')
 
-COLLECTIONS = int(os.getenv("COLLECTIONS",default = 5))
-SIZE = int(os.getenv("SIZE",default = 10))
+collections = int(os.getenv("COLLECTIONS", default = 5))
+datasize = int(os.getenv("DATASIZE", default = 100))
+documentCount = datasize / collections
 TIMEOUT = int(os.getenv("TIMEOUT",default = 300))
-STORAGE = os.getenv("STORAGE")
-BACKUP_TYPE = os.getenv("BACKUP_TYPE")
-EXISTING_BACKUP = os.getenv("EXISTING_BACKUP",default = "no")
-CHECK_PITR = os.getenv("CHECK_PITR",default = "yes")
-numDownloadWorkers = os.getenv("RESTORE_NUMDOWNLOADWORKERS",default = '0')
-maxDownloadBufferMb = os.getenv("RESTORE_NUMDOWNLOADBUFFERMB",default = '0')
-downloadChunkMb = os.getenv("RESTORE_DOWNLOADCHUNKMB",default = '0')
 
-def create_config():
+def create_config(documentCount, collections):
     string = []
-    for x in range(COLLECTIONS):
+    for x in range(collections):
         collectionName = f"collection{x}"
-        string2 = {'database': 'test','collection': collectionName,'count': SIZE,'content': {'binary': {'type': 'binary','minLength': 1048576, 'maxLength': 1048576}}}
+        string2 = {'database': 'test','collection': collectionName,'count': documentCount,'content': {'binary': {'type': 'binary','minLength': 10485760, 'maxLength': 10485760}}}
         string.append(string2)
-
     return string
 
 def pytest_configure():
     pytest.backup_name = ''
     pytest.pitr_start = ''
     pytest.pitr_end = ''
-
-def find_backup(node,port,name):
-    list = node.check_output('pbm list --mongodb-uri=mongodb://localhost:' + port + '/ --out=json')
-    parsed_list = json.loads(list)
-    if parsed_list['snapshots']:
-        for snapshot in parsed_list['snapshots']:
-            if snapshot['name'] == name:
-                return snapshot
-                break
 
 def find_event_msg(node,port,event,msg):
     command = "pbm logs --mongodb-uri=mongodb://localhost:" + port + "/ --tail=100 --out=json --event=" + event
@@ -56,37 +40,9 @@ def find_event_msg(node,port,event,msg):
              return log
              break
 
-def get_pbm_logs(node,port):
-    command = "pbm logs -s D --mongodb-uri=mongodb://localhost:" + port + "/ --tail=0"
-    logs = node.check_output(command)
-    print(logs)
-
-def check_status(node,port):
-    status = node.check_output('pbm status --mongodb-uri=mongodb://localhost:' + port + '/ --out=json 2>/dev/null')
-    running = json.loads(status)['running']
-    if running:
-        return running
-
-def check_pitr(node,port):
-    status = node.check_output('pbm status --mongodb-uri=mongodb://localhost:' + port + '/ --out=json 2>/dev/null')
-    running = json.loads(status)['pitr']['run']
-    return bool(running)
-
-def check_agents_status(node,port):
-    result = node.check_output('pbm status --mongodb-uri=mongodb://localhost:' + port + '/ --out=json')
-    parsed_result = json.loads(result)
-    for replicaset in parsed_result['cluster']:
-        for host in replicaset['nodes']:
-            assert host['ok'] == True
-
 def check_mongod_service(node):
     with node.sudo():
         service = node.service("mongod")
-        assert service.is_running
-
-def check_pbm_service(node):
-    with node.sudo():
-        service = node.service("pbm-agent")
         assert service.is_running
 
 def restart_mongod(node):
@@ -95,112 +51,8 @@ def restart_mongod(node):
         result = node.check_output('systemctl restart mongod')
     print('restarting mongod on ' + hostname)
 
-def restart_pbm_agent(node):
-    with node.sudo():
-        hostname = node.check_output('hostname -s')
-        result = node.check_output('systemctl restart pbm-agent')
-    print('restarting pbm-agent on ' + hostname)
-
-def make_backup(node,port,type):
-    for i in range(TIMEOUT):
-        running = check_status(node,port)
-        if not running:
-            if type == 'physical':
-                start = node.check_output('pbm backup --mongodb-uri=mongodb://localhost:' + port + '/ --out=json --type=' + type )
-            else:
-                start = node.check_output('pbm backup --mongodb-uri=mongodb://localhost:' + port + '/ --out=json')
-            name = json.loads(start)['name']
-            print("backup started:")
-            print(name)
-            break
-        else:
-            print("unable to start backup - another operation in work")
-            print(running)
-            time.sleep(1)
-    for i in range(TIMEOUT):
-        running = check_status(node,port)
-        print("current operation:")
-        print(running)
-        result = find_backup(node,port,name)
-        if result:
-            print("backup found:")
-            print(result)
-            assert result['status'] == 'done'
-            return name
-            break
-        else:
-            time.sleep(1)
-
-def make_restore(node,port,name):
-    for i in range(TIMEOUT):
-        running = check_status(node,port)
-        if not running:
-            output = node.check_output('pbm restore --mongodb-uri=mongodb://localhost:' + port + '/ ' + name + ' --wait')
-            print(output)
-            break
-        else:
-            print("unable to start restore - another operation in work")
-            print(running)
-            time.sleep(1)
-    for i in [secondary1_rs, secondary2_rs, primary_rs]:
-        restart_mongod(i)
-        time.sleep(5)
-    time.sleep(5)
-    for i in [secondary1_rs, secondary2_rs, primary_rs]:
-        restart_pbm_agent(i)
-        time.sleep(5)
-    for i in [secondary1_rs, secondary2_rs, primary_rs]:
-        with i.sudo():
-            hostname = i.check_output('hostname -s')
-            logs = i.check_output('journalctl -u pbm-agent -b -o cat --no-pager | grep restore | grep download || true')
-            print("logs from " + hostname)
-            print(logs)
-    output = node.check_output('pbm config --mongodb-uri=mongodb://localhost:' + port + '/ --force-resync')
-    print(output)
-    for i in range(TIMEOUT):
-        logs = find_event_msg(node,"27017","resync","succeed")
-        if logs:
-            print(logs)
-            break
-        else:
-            time.sleep(1)
-    for i in [secondary1_rs, secondary2_rs, primary_rs]:
-        check_mongod_service(i)
-
-def make_pitr_restore(node,port,name,timestamp):
-    for i in range(TIMEOUT):
-        running = check_status(node,port)
-        if not running:
-            output = node.check_output('pbm restore --mongodb-uri=mongodb://localhost:' + port + '/ --time=' + timestamp + ' --base-snapshot=' + name + ' --wait')
-            print(output)
-            break
-        else:
-            print("unable to start restore - another operation in work")
-            print(running)
-            time.sleep(1)
-    for i in [secondary1_rs, secondary2_rs, primary_rs]:
-        restart_mongod(i)
-        time.sleep(5)
-    time.sleep(5)
-    for i in [secondary1_rs, secondary2_rs, primary_rs]:
-        restart_pbm_agent(i)
-        time.sleep(5)
-    time.sleep(5)
-
-def make_pitr_replay(node,port,start,end):
-    for i in range(TIMEOUT):
-        running = check_status(node,port)
-        if not running:
-            output = node.check_output('pbm oplog-replay --mongodb-uri=mongodb://localhost:' + port + '/ --start=' + start + ' --end=' + end + ' --wait')
-            print(output)
-            break
-        else:
-            print("unable to start restore - another operation in work")
-            print(running)
-            time.sleep(1)
-
 def load_data(node,port):
-    config = create_config()
+    config = create_config(documentcount, documentsize, collections)
     config_json = json.dumps(config, indent=4)
     node.run_test('echo \'' + config_json + '\' > /tmp/generated_config.json')
     node.check_output('mgodatagen --uri=mongodb://127.0.0.1:' + port + '/?replicaSet=rs -f /tmp/generated_config.json --batchsize 10')
