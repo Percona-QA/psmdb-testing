fefce38039ae54bc66fe978ad61844ebc92ff9c5
Keith Quinn <keith.quinn@percona.com>
2025-04-19 10:47:57 +0100

    Testing Data Integrity updates

--- pml-functional/replicaset/tests/test_pml.py
+++ pml-functional/replicaset/tests/test_pml.py
@@ -10,7 +10,7 @@ import urllib3
 import json
 import testinfra.utils.ansible_runner
 from mlink.data_integrity_check import compare_data_rs
-sys.path.append('../')
+sys.path.append('../../../')
 
 source = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_host('jenkins-pml-source')
