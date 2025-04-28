cf1ab218f5587042d08ecf03127613bde5470940
Keith Quinn <keith.quinn@percona.com>
2025-04-28 11:48:19 +0100

    Updating function for starting Mongolink

--- pml-functional/replicaset/tests/test_pml.py
+++ pml-functional/replicaset/tests/test_pml.py
@@ -224,17 +224,17 @@ def wait_for_repl_stage(timeout=60, interval=1, stable_duration=2):
 #     print("PML did not finalize after " + str(timeout) + " seconds.")
 #     return False
 
-def test_prepare_data():
-    load_data(source,"27017")
-    assert confirm_collection_size(source, "27017", collections, datasize)
-
-def test_initiate_pml():
-    result = json.loads(pml.check_output(
-        "percona-mongolink start"))
-    assert result in [{"ok": True}, {'error': 'already running', 'ok': False}]
-    assert pml_start()
-    assert wait_for_repl_stage()
-    assert pml_finalize()
+# def test_prepare_data():
+#     load_data(source,"27017")
+#     assert confirm_collection_size(source, "27017", collections, datasize)
+#
+# def test_initiate_pml():
+#     result = json.loads(pml.check_output(
+#         "percona-mongolink start"))
+#     assert result in [{"ok": True}, {'error': 'already running', 'ok': False}]
+#     assert pml_start()
+#     assert wait_for_repl_stage()
+#     assert pml_finalize()
 
 def test_data_transfer():
     assert confirm_collection_size(destination, "27017", collections, datasize)
