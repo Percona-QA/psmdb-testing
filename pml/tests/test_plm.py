4d498d186cbbc730cfac3b32f349f953da3350fa
Keith Quinn <keith.quinn@percona.com>
2025-05-08 15:52:44 +0100

    Fixing tests

--- pml/tests/test_plm.py
+++ pml/tests/test_plm.py
@@ -23,16 +23,16 @@ def plm_start(host):
     assert "Change Replication started" in result.stdout
     return True
 
-# @pytest.fixture()
-# def pml_status(host):
-#     """Start and stop pbm-agent service
-#
-#     :param host:
-#     :return:
-#     """
-#     result = host.run("percona-mongolink status")
-#     assert result.rc == 0, result.stdout
-#     return result
+@pytest.fixture()
+def pml_status(host):
+    """Start and stop pbm-agent service
+
+    :param host:
+    :return:
+    """
+    result = host.run("percona-mongolink status")
+    assert result.rc == 0, result.stdout
+    return result
 #
 # @pytest.fixture()
 # def plm_finalize(host):
@@ -56,30 +56,30 @@ def plm_start(host):
 #     except AssertionError:
 #         pytest.xfail("Possible xfail")
 #
-def test_plm_start(plm_start):
-    assert plm_start
-#
-# def test_plm_status(pml_status):
-#     pml_status_output = json.loads(pml_status.stdout)
-#     assert "ok" in pml_status_output
-#     assert "state" in pml_status_output
-#     assert "info" in pml_status_output
-#     assert "lagTime" in pml_status_output
-#     assert "eventsProcessed" in pml_status_output
-#     assert "lastReplicatedOpTime" in pml_status_output
-#     assert "initialSync" in pml_status_output
-#
-#     assert pml_status_output["ok"] is True
-#     assert pml_status_output["state"] == "running"
-#     assert pml_status_output["info"] == "Replicating Changes"
-#     assert pml_status_output["lagTime"] >= 0
-#     assert isinstance(pml_status_output["eventsProcessed"], int)
-#
-#     sync = pml_status_output["initialSync"]
-#     assert sync["completed"] is True
-#     assert sync["cloneCompleted"] is True
-#     assert sync["estimatedCloneSize"] >= 0
-#     assert sync["clonedSize"] == sync["estimatedCloneSize"]
+# def test_plm_start(plm_start):
+#     assert plm_start
+
+def test_plm_status(pml_status):
+    pml_status_output = json.loads(pml_status.stdout)
+    assert "ok" in pml_status_output
+    assert "state" in pml_status_output
+    assert "info" in pml_status_output
+    assert "lagTime" in pml_status_output
+    assert "eventsProcessed" in pml_status_output
+    assert "lastReplicatedOpTime" in pml_status_output
+    assert "initialSync" in pml_status_output
+
+    assert pml_status_output["ok"] is True
+    assert pml_status_output["state"] == "running"
+    assert pml_status_output["info"] == "Replicating Changes"
+    assert pml_status_output["lagTime"] >= 0
+    assert isinstance(pml_status_output["eventsProcessed"], int)
+
+    sync = pml_status_output["initialSync"]
+    assert sync["completed"] is True
+    assert sync["cloneCompleted"] is True
+    assert sync["estimatedCloneSize"] >= 0
+    assert sync["clonedSize"] == sync["estimatedCloneSize"]
 #
 #
 # # TODO add correct start/stop test
