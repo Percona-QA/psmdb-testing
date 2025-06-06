1d27fa0247022efce3830168b7227835ed391051
Keith Quinn <keith.quinn@percona.com>
2025-06-06 10:01:05 +0100

    Adding branch

---
+++ pbm-functional/pytest/test_PBM-1544.py
@@ -0,0 +1,54 @@
+import pytest
+import pymongo
+import bson
+import testinfra
+import time
+import os
+import docker
+import concurrent.futures
+import random
+import json
+
+from datetime import datetime
+from cluster import Cluster
+from packaging import version
+
+documents=[{"a": 1}, {"b": 2}, {"c": 3}, {"d": 4}]
+
+@pytest.fixture(scope="package")
+def docker_client():
+    return docker.from_env()
+
+@pytest.fixture(scope="package")
+def config():
+    return { "_id": "rs1", "members": [{"host":"rs101"},{"host": "rs102"},{"host": "rs103" }]}
+
+@pytest.fixture(scope="package")
+def cluster(config):
+    return Cluster(config)
+
+@pytest.fixture(scope="function")
+def start_cluster(cluster,request):
+    try:
+        cluster.destroy()
+        cluster.create()
+        cluster.setup_pbm()
+        os.chmod("/backups",0o777)
+        os.system("rm -rf /backups/*")
+        yield True
+    finally:
+        if request.config.getoption("--verbose"):
+            cluster.get_logs()
+        cluster.destroy(cleanup_backups=True)
+
+
+@pytest.mark.timeout(300,func_only=True)
+def test_physical(start_cluster,cluster):
+    cluster.check_pbm_status()
+    pymongo.MongoClient(cluster.connection)["test"]["test"].insert_many(documents)
+    backup=cluster.make_backup("physical")
+    result=pymongo.MongoClient(cluster.connection)["test"]["test"].delete_many({})
+    assert int(result.deleted_count) == len(documents)
+    cluster.make_restore(backup,restart_cluster=True, check_pbm_status=True)
+    assert pymongo.MongoClient(cluster.connection)["test"]["test"].count_documents({}) == len(documents)
+    Cluster.log("Finished successfully")
