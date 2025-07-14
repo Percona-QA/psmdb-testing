69c4abb9b07486fd2a7aa3e93aa30063afbd9404
Keith Quinn <keith.quinn@percona.com>
2025-07-14 08:33:59 +0100

    PBM Test

--- pbm-functional/replicaset/tests/test_pbm_replicaset.py
+++ pbm-functional/replicaset/tests/test_pbm_replicaset.py
@@ -311,30 +311,29 @@ def setup_pitr(node,port):
 #     pytest.pitr_end = now.strftime("%Y-%m-%dT%H:%M:%S")
 #     print("pitr end time: " + pytest.pitr_end)
 
-def test_7_disable_pitr():
-    if EXISTING_BACKUP != "no" or CHECK_PITR == "no":
-        pytest.skip("Skipping pitr test")
-    result = primary_rs.check_output('pbm config --mongodb-uri=mongodb://localhost:27017/ --set pitr.enabled=false --out=json')
-    store_out = json.loads(result)
-    print(store_out)
-    time.sleep(60)
-    for i in range(TIMEOUT):
-        pitr = check_pitr(primary_rs,"27017")
-        if pitr:
-            time.sleep(1)
-            print("waiting for pitr to be disabled")
-        else:
-            print("pitr disabled")
-            break
-    assert check_pitr(primary_rs,"27017") == False
+# def test_7_disable_pitr():
+#     if EXISTING_BACKUP != "no" or CHECK_PITR == "no":
+#         pytest.skip("Skipping pitr test")
+#     result = primary_rs.check_output('pbm config --mongodb-uri=mongodb://localhost:27017/ --set pitr.enabled=false --out=json')
+#     store_out = json.loads(result)
+#     print(store_out)
+#     time.sleep(60)
+#     for i in range(TIMEOUT):
+#         pitr = check_pitr(primary_rs,"27017")
+#         if pitr:
+#             time.sleep(1)
+#             print("waiting for pitr to be disabled")
+#         else:
+#             print("pitr disabled")
+#             break
+#     assert check_pitr(primary_rs,"27017") == False
 #
-# def test_8_restore():
-#     if EXISTING_BACKUP != "no":
-#         pytest.backup_name = EXISTING_BACKUP
-#     time.sleep(3600)
-#     make_restore(secondary1_rs,"27017",pytest.backup_name)
-#     count = check_count_data(primary_rs,"27017")
-#     assert int(count) == SIZE
+def test_8_restore():
+    if EXISTING_BACKUP != "no":
+        pytest.backup_name = EXISTING_BACKUP
+    make_restore(secondary1_rs,"27017","2025-07-14T07:15:53Z")
+    count = check_count_data(primary_rs,"27017")
+    assert int(count) == SIZE
 
 # def test_9_pitr_restore():
 #     if EXISTING_BACKUP != "no" or CHECK_PITR == "no":
