ccc83c48c631936164efa1ed0fb99d9123663f4a
Keith Quinn <keith.quinn@percona.com>
2025-07-23 15:09:41 +0100

    PBM Test

--- pbm-functional/replicaset/tests/test_pbm_replicaset.py
+++ pbm-functional/replicaset/tests/test_pbm_replicaset.py
@@ -275,26 +275,26 @@ def test_3_prepare_data():
     count = check_count_data(primary_rs,"27017")
     assert int(count) == SIZE
 
-def test_4_setup_pitr():
-    if EXISTING_BACKUP != "no" or CHECK_PITR == "no":
-        pytest.skip("Skipping pitr test")
-    if BACKUP_TYPE == "physical":
-        pytest.backup_name = make_backup(primary_rs, "27017", BACKUP_TYPE)
-        print(str(pytest.backup_name))
-        result = primary_rs.check_output('pbm config --mongodb-uri=mongodb://localhost:27017/ --set pitr.enabled=true --set pitr.oplogOnly=true --out=json')
-        for i in range(TIMEOUT):
-            pitr = check_pitr(primary_rs,"27017")
-            if not pitr:
-                print("waiting for pitr to be enabled")
-                time.sleep(1)
-            else:
-                print("pitr enabled")
-                break
-        assert check_pitr(primary_rs,"27017") == True
-    else:
-        result = primary_rs.check_output('pbm config --mongodb-uri=mongodb://localhost:27017/ --set pitr.enabled=true --out=json')
-    store_out = json.loads(result)
-    print(store_out)
+# def test_4_setup_pitr():
+#     if EXISTING_BACKUP != "no" or CHECK_PITR == "no":
+#         pytest.skip("Skipping pitr test")
+#     if BACKUP_TYPE == "physical":
+#         pytest.backup_name = make_backup(primary_rs, "27017", BACKUP_TYPE)
+#         print(str(pytest.backup_name))
+#         result = primary_rs.check_output('pbm config --mongodb-uri=mongodb://localhost:27017/ --set pitr.enabled=true --set pitr.oplogOnly=true --out=json')
+#         for i in range(TIMEOUT):
+#             pitr = check_pitr(primary_rs,"27017")
+#             if not pitr:
+#                 print("waiting for pitr to be enabled")
+#                 time.sleep(1)
+#             else:
+#                 print("pitr enabled")
+#                 break
+#         assert check_pitr(primary_rs,"27017") == True
+#     else:
+#         result = primary_rs.check_output('pbm config --mongodb-uri=mongodb://localhost:27017/ --set pitr.enabled=true --out=json')
+#     store_out = json.loads(result)
+#     print(store_out)
 
 def test_5_backup():
     if EXISTING_BACKUP != "no":
@@ -326,22 +326,22 @@ def test_6_modify_data():
     pytest.pitr_end = now.strftime("%Y-%m-%dT%H:%M:%S")
     print("pitr end time: " + pytest.pitr_end)
 
-def test_7_disable_pitr():
-    if EXISTING_BACKUP != "no" or CHECK_PITR == "no":
-        pytest.skip("Skipping pitr test")
-    result = primary_rs.check_output('pbm config --mongodb-uri=mongodb://localhost:27017/ --set pitr.enabled=false --out=json')
-    store_out = json.loads(result)
-    print(store_out)
-    time.sleep(60)
-    for i in range(TIMEOUT):
-        pitr = check_pitr(primary_rs,"27017")
-        if pitr:
-            time.sleep(1)
-            print("waiting for pitr to be disabled")
-        else:
-            print("pitr disabled")
-            break
-    assert check_pitr(primary_rs,"27017") == False
+# def test_7_disable_pitr():
+#     if EXISTING_BACKUP != "no" or CHECK_PITR == "no":
+#         pytest.skip("Skipping pitr test")
+#     result = primary_rs.check_output('pbm config --mongodb-uri=mongodb://localhost:27017/ --set pitr.enabled=false --out=json')
+#     store_out = json.loads(result)
+#     print(store_out)
+#     time.sleep(60)
+#     for i in range(TIMEOUT):
+#         pitr = check_pitr(primary_rs,"27017")
+#         if pitr:
+#             time.sleep(1)
+#             print("waiting for pitr to be disabled")
+#         else:
+#             print("pitr disabled")
+#             break
+#     assert check_pitr(primary_rs,"27017") == False
 
 def test_8_restore():
     if EXISTING_BACKUP != "no":
