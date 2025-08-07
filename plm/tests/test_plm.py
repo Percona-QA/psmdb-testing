ab8c1524755cb3275ebf2c950aedd6975c2c206b
Keith Quinn <keith.quinn@percona.com>
2025-08-07 16:43:15 +0100

    Debugging tests

--- plm/tests/test_plm.py
+++ plm/tests/test_plm.py
@@ -186,7 +186,6 @@ def get_git_commit():
     git_commit = requests.get(url, headers=headers)
 
     if git_commit.status_code == 200:
-        print("KEITH TEST SHA: " + str(git_commit.json()["sha"]))
         return git_commit.json()["sha"]
     else:
         print(f"Unable to obtain git commit, failed with status code: {git_commit.status_code}")
@@ -205,47 +204,47 @@ def test_plm_version(host):
     assert parsed_config['BuildTime'], parsed_config
     assert parsed_config['GoVersion'], parsed_config
 
-# def test_plm_binary(host):
-#     """Check PLM binary exists with the correct permissions"""
-#     file = host.file("/usr/bin/plm")
-#     assert file.user == "root"
-#     assert file.group == "root"
-#     try:
-#         assert file.mode == 0o755
-#     except AssertionError:
-#         pytest.xfail("Possible xfail")
-#
-# def test_plm_help(host):
-#     """Check that PLM help command works"""
-#     result = host.run("plm help")
-#     assert result.rc == 0, result.stdout
-#
-# def test_plm_environment_file_exists(host):
-#     """Test plm-service file exists"""
-#     service_file = host.file("/lib/systemd/system/plm.service")
-#     assert service_file.user == "root"
-#     assert service_file.group == "root"
-#     try:
-#         assert service_file.mode == 0o644
-#     except AssertionError:
-#         pytest.xfail("Possible xfail")
-#
-# def test_stop_plm(host):
-#     """Test plm service stops successfully"""
-#     stop_plm_service(host)
-#
-# def test_start_plm(host):
-#     """Test plm service starts successfully"""
-#     start_plm_service(host)
-#
-# def test_restart_plm(host):
-#     """Test plm service restarts successfully"""
-#     restart_plm_service(host)
-#
-# def test_plm_transfer(host):
-#     """Test basic PLM Transfer functionality"""
-#     assert plm_add_db_row(host)
-#     assert plm_start(host)
-#     assert wait_for_repl_stage(host)
-#     assert "testUser" in plm_confirm_db_row(host).stdout
-#     assert plm_finalize(host)
+def test_plm_binary(host):
+    """Check PLM binary exists with the correct permissions"""
+    file = host.file("/usr/bin/plm")
+    assert file.user == "root"
+    assert file.group == "root"
+    try:
+        assert file.mode == 0o755
+    except AssertionError:
+        pytest.xfail("Possible xfail")
+
+def test_plm_help(host):
+    """Check that PLM help command works"""
+    result = host.run("plm help")
+    assert result.rc == 0, result.stdout
+
+def test_plm_environment_file_exists(host):
+    """Test plm-service file exists"""
+    service_file = host.file("/lib/systemd/system/plm.service")
+    assert service_file.user == "root"
+    assert service_file.group == "root"
+    try:
+        assert service_file.mode == 0o644
+    except AssertionError:
+        pytest.xfail("Possible xfail")
+
+def test_stop_plm(host):
+    """Test plm service stops successfully"""
+    stop_plm_service(host)
+
+def test_start_plm(host):
+    """Test plm service starts successfully"""
+    start_plm_service(host)
+
+def test_restart_plm(host):
+    """Test plm service restarts successfully"""
+    restart_plm_service(host)
+
+def test_plm_transfer(host):
+    """Test basic PLM Transfer functionality"""
+    assert plm_add_db_row(host)
+    assert plm_start(host)
+    assert wait_for_repl_stage(host)
+    assert "testUser" in plm_confirm_db_row(host).stdout
+    assert plm_finalize(host)
