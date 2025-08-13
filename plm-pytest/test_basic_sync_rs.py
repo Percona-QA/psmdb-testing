bdf4250ec731b0a194758053b9d64fa1650fba24
Keith Quinn <keith.quinn@percona.com>
2025-08-13 12:31:27 +0100

    Testing PLM Github action

--- plm-pytest/test_basic_sync_rs.py
+++ plm-pytest/test_basic_sync_rs.py
@@ -71,7 +71,7 @@ def reset_state(srcRS, dstRS, plink, request):
 @pytest.mark.usefixtures("start_cluster")
 def test_rs_plink_PML_T2(reset_state, srcRS, dstRS, plink, metrics_collector):
     """
-    Test to validate basic sync of all data types including time-series. Data is added before sync,
+    Test test to validate basic sync of all data types including time-series. Data is added before sync,
     during data clone and replication phase, also CRUD operations are performed all time during the sync.
     """
     try:
