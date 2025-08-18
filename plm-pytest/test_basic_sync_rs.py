5b15a24a5c0d10522c9af286c59b4d69b3fa779b
Keith Quinn <keith.quinn@percona.com>
2025-08-18 10:20:45 +0100

    Fixing metrics_collector import ruff issue and unused variable

--- plm-pytest/test_basic_sync_rs.py
+++ plm-pytest/test_basic_sync_rs.py
@@ -72,7 +72,6 @@ def check_logs(search_line, plink, expected_count):
     logs = plink.logs(tail=1000)
     for log in logs.splitlines():
         if search_line in log:
-            print("\n\n\nKEITH TEST: " + log + "\n\n\n")
             count += 1
     if count == expected_count:
         return True
@@ -672,7 +671,7 @@ def test_rs_plink_PML_T30(reset_state, srcRS, dstRS, plink):
     Test to validate handling of concurrent data clone and index build failure
     """
     src = pymongo.MongoClient(srcRS.connection)
-    dst = pymongo.MongoClient(dstRS.connection)
+    pymongo.MongoClient(dstRS.connection)
     normal_docs = [{"a": {"b": 1}, "words": "omnibus"} for _ in range(20000)]
     src["init_test_db"].invalid_text_collection1.insert_many(normal_docs)
     src["init_test_db"].invalid_text_collection1.insert_one({"a": {"b": []}, "words": "omnibus"})
