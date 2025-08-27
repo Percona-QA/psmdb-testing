5404154a39416653da4d8bec13b47f6b55ceca3e
keithquinnpercona <keith.quinn@percona.com>
2025-08-27 08:15:54 +0100

    PBM. Add test for different storage providers and encryption types

---
+++ pbm-functional/pytest/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption("--jenkins", action="store_true", default=False, help="Run tests marked as jenkins")
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--jenkins"):
+        return
+    skip_jenkins = pytest.mark.skip(reason="Skipped because --jenkins flag not set")
+    for item in items:
+        if "jenkins" in item.keywords:
+            item.add_marker(skip_jenkins)
