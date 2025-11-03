ada3158b121e8d5fd001ca9b0cde291ebb6db953
Keith Quinn <keith.quinn@percona.com>
2025-11-03 10:20:39 +0000

    Debugging tests

--- psmdb-tarball/psmdb-tarball/tests/test_psmdb_tarball.py
+++ psmdb-tarball/psmdb-tarball/tests/test_psmdb_tarball.py
@@ -14,13 +14,13 @@ BINARIES = ['mongod', 'mongos', 'bsondump', 'mongoexport', 'mongobridge',
 tarball_url = os.environ["TARBALL"]
 match_exp_version = re.search(r'server-mongodb-(\d+\.\d+\.\d+-\d+)', tarball_url)
 
-JSTESTS = ['test_kerberos_simple.js','test_ldap_simple.js']
-SUITES = ['multiversion_kmip', 'multiversion_vault']
+JSTESTS = ['test_kerberos_simple.js']
+# SUITES = ['multiversion_kmip', 'multiversion_vault']
 
-@pytest.mark.parametrize("binary", BINARIES)
-def test_binary_version(host, binary):
-    result = host.check_output(f"/usr/bin/{binary} --version")
-    assert match_exp_version.group(1) in result, f"{result}"
+# @pytest.mark.parametrize("binary", BINARIES)
+# def test_binary_version(host, binary):
+#     result = host.check_output(f"/usr/bin/{binary} --version")
+#     assert match_exp_version.group(1) in result, f"{result}"
 
 @pytest.mark.parametrize("jstest", JSTESTS)
 def test_jstests(host, jstest):
@@ -30,10 +30,10 @@ def test_jstests(host, jstest):
         print(result.stderr)
     assert result.rc == 0, result.stdout
 
-@pytest.mark.parametrize("suites", SUITES)
-def test_suites(host, suites):
-    cmd = "cd /percona-server-mongodb && /opt/venv/bin/python buildscripts/resmoke.py run --suite "  + suites
-    with host.sudo():
-        result = host.run(cmd)
-        print(result.stderr)
-    assert result.rc == 0, result.stdout
+# @pytest.mark.parametrize("suites", SUITES)
+# def test_suites(host, suites):
+#     cmd = "cd /percona-server-mongodb && /opt/venv/bin/python buildscripts/resmoke.py run --suite "  + suites
+#     with host.sudo():
+#         result = host.run(cmd)
+#         print(result.stderr)
+#     assert result.rc == 0, result.stdout
