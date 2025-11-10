081ddc502f3cd34045e877fcf61554fdd6c20e6c
keithquinnpercona <keith.quinn@percona.com>
2025-11-10 11:13:40 +0000

    PSMDB-1777: Add Keycloak role and OIDC Test

---
+++ jstests/test_oidc_simple.js
@@ -0,0 +1,76 @@
+(function() {
+    'use strict';
+
+    var conn = MongoRunner.runMongod();
+
+    var admin = conn.getDB("admin");
+
+    admin.createRole({
+        role: "keycloak/Everyone", privileges: [],
+        roles: [ "readWriteAnyDatabase"]
+    });
+
+    admin.logout()
+
+    var ext = conn.getDB("$external");
+
+    ext.createUser({
+        user: "keycloak/pbmclient",
+        roles: [ { role: "keycloak/Everyone", db: "admin" } ]
+    });
+
+    ext.logout()
+
+    MongoRunner.stopMongod(conn);
+
+    var oidcProviders = JSON.stringify(
+        [
+                {
+                    issuer: "https://keycloak:8443/realms/test",
+                    clientId: "pbmclient",
+                    audience: "account",
+                    authNamePrefix: "keycloak",
+                    useAuthorizationClaim: false,
+                    supportsHumanFlows: false,
+                    principalName: "client_id"
+                }
+        ]
+    );
+
+    // test command line parameters related to OIDC authorization
+    conn = MongoRunner.runMongod({
+        restart: conn,
+        auth: '',
+        setParameter: {authenticationMechanisms: 'SCRAM-SHA-1,MONGODB-OIDC', oidcIdentityProviders: oidcProviders},
+        noCleanData: true
+    });
+
+    assert(conn, "Cannot start mongod instance");
+
+    const token_raw = MongoRunner.dataPath + "oidc_token.txt";
+
+    assert.eq(0, runProgram("bash", "-lc",
+      "curl --silent --show-error " +
+      "-X POST 'https://keycloak:8443/realms/test/protocol/openid-connect/token' " +
+      "-d 'grant_type=client_credentials&client_id=pbmclient&client_secret=pbm-secret' " +
+      "| jq -r .access_token > " + token_raw
+    ));
+
+    const token = cat(token_raw).trim();
+
+    var clientConnect = function(conn) {
+        const exitCode = runMongoProgram("/percona-server-mongodb/mongo",
+                                         "--quiet",
+                                         "--port", conn.port,
+                                         "--authenticationDatabase", '$external',
+                                         "--authenticationMechanism", "MONGODB-OIDC",
+                                         "--oidcAccessToken", token,
+                                         "--eval",
+                                         "db.runCommand({connectionStatus: 1});");
+        return exitCode;
+    };
+
+    assert.eq(clientConnect(conn), 0);
+
+    MongoRunner.stopMongod(conn);
+})();
