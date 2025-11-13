1f9f509b44d508a0d5eff4d3469dbe5251cb9c11
Keith Quinn <keith.quinn@percona.com>
2025-11-13 09:20:49 +0000

    Adding FIPs and cleanup to non pro tests

--- psmdb-tarball/psmdb-tarball/tests/test_psmdb_tarball.py
+++ psmdb-tarball/psmdb-tarball/tests/test_psmdb_tarball.py
@@ -12,15 +12,15 @@ BINARIES = ['mongod', 'mongos', 'bsondump', 'mongoexport', 'mongobridge',
             'mongofiles', 'mongoimport', 'mongorestore', 'mongotop', 'mongostat']
 
 psmdb_version = os.environ["PSMDB_VERSION"]
-match_exp_version = re.search(r'(\d+\.\d+\.\d+-\d+)', psmdb_version)
 
 JSTESTS = ['test_kerberos_simple.js','test_ldap_simple.js', 'test_oidc_simple.js']
 SUITES = ['multiversion_kmip', 'multiversion_vault']
+FIPS = ['ssl jstests/ssl/ssl_fips.js']
 
 @pytest.mark.parametrize("binary", BINARIES)
 def test_binary_version(host, binary):
     result = host.check_output(f"/usr/bin/{binary} --version")
-    assert match_exp_version.group(1) in result, f"{result}"
+    assert psmdb_version in result, f"{result}"
 
 @pytest.mark.parametrize("jstest", JSTESTS)
 def test_jstests(host, jstest):
@@ -32,7 +32,19 @@ def test_jstests(host, jstest):
 
 @pytest.mark.parametrize("suites", SUITES)
 def test_suites(host, suites):
+    if host.system_info.distribution == "debian" or (host.system_info.distribution == "ubuntu" and "24.04" in host.system_info.release):
+        pytest.skip("Skip debian12 as no openssl with FIPS available")
     cmd = "cd /percona-server-mongodb && /opt/venv/bin/python buildscripts/resmoke.py run --suite "  + suites
+    with host.sudo():
+        result = host.run(cmd)
+        print(result.stderr)
+    assert result.rc == 0, result.stdout
+
+@pytest.mark.parametrize("fips", FIPS)
+def test_fips(host, fips):
+    if host.system_info.distribution == "debian" or (host.system_info.distribution == "ubuntu" and "24.04" in host.system_info.release):
+        pytest.skip("Skip debian12 as no openssl with FIPS available")
+    cmd = "cd /percona-server-mongodb && /opt/venv/bin/python buildscripts/resmoke.py run --suite "  + FIPS
     with host.sudo():
         result = host.run(cmd)
         print(result.stderr)
