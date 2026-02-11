aa3b9ecfab9befc02323023947029341b0683bd7
Keith Quinn <keith.quinn@percona.com>
2026-02-11 11:50:31 +0000

    Adding tests for PCSM-253

--- pcsm-pytest/test_arguments_and_env_vars.py
+++ pcsm-pytest/test_arguments_and_env_vars.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 import pytest
 
@@ -213,6 +214,53 @@ def test_clone_read_batch_size_PML_T74(start_cluster, src_cluster, dst_cluster, 
     assert check_command_output(expected_cmd_return, csync, should_pass)
     assert expected_log in csync.logs(tail=3000), f"Expected {expected_log} does not appear in logs"
 
+@pytest.mark.parametrize("cluster_configs", ["replicaset"], indirect=True)
+@pytest.mark.timeout(2700, func_only=True)
+@pytest.mark.parametrize("raw_args, should_pass, expected_cmd_return, expected_log, mode", [
+                            (["--use-collection-bulk-write=True"], True, '"ok": true', "Use collection-level bulk write", "cli"),
+                            (["--use-collection-bulk-write=False"], True, '"ok": true', "", "cli"),
+                            (["--use-collection-bulk-write true"], True, '"ok": true', "Use collection-level bulk write", "cli"),
+                            (["--use-collection-bulk-write false"], True, '"ok": true', "", "cli"),
+                            (["--use-collection-bulk-write"], True, '"ok": true', "Use collection-level bulk write", "cli"),
+                            (["--use-collection-bulk-write", "--use-collection-bulk-write=false"], True, '"ok": true', "", "cli"),
+                            (["--use-collection-bulk-write=false", "--use-collection-bulk-write"], True, '"ok": true', "Use collection-level bulk write", "cli"),
+                            (["--use-collection-bulk-write= "], False, 'Error: invalid argument "" for "--use-collection-bulk-write" flag: strconv.ParseBool: parsing "": invalid syntax', "", "cli"),
+                            (["--use-collection-bulk-write==true"], False, 'Error: invalid argument "=true" for "--use-collection-bulk-write" flag: strconv.ParseBool: parsing "=true": invalid syntax', "", "cli"),
+                            (["--use-collection-bulk-write=f"], True, '"ok": true', "", "cli"),
+                            (["--use-collection-bulk-write=test"], False, 'Error: invalid argument "test" for "--use-collection-bulk-write" flag: strconv.ParseBool: parsing "test": invalid syntax', "", "cli"),
+                            ({"useCollectionBulkWrite": True}, True, '"ok": true', "Use collection-level bulk write", "http"),
+                            ({"useCollectionBulkWrite": 123}, False, 'Error: invalid argument "123" for "--use-collection-bulk-write" flag: strconv.ParseBool: parsing "123": invalid syntax', "", "http"),
+])
+def test_use_collection_bulk_write_PML_T70(start_cluster, src_cluster, dst_cluster, csync, raw_args, should_pass, expected_cmd_return, expected_log, mode):
+    """
+    Test PCSM --use-collection-bulk-write argument and useCollectionBulkWrite environment variable
+    """
+    try:
+        _, operation_threads_1 = create_all_types_db(src_cluster.connection, "init_test_db", start_crud=True, is_sharded=src_cluster.is_sharded)
+        _, operation_threads_2 = create_all_types_db(src_cluster.connection, "clone_test_db", start_crud=True, is_sharded=src_cluster.is_sharded)
+        _, operation_threads_3 = create_all_types_db(src_cluster.connection, "repl_test_db", start_crud=True, is_sharded=src_cluster.is_sharded)
+        assert csync.start(mode=mode, raw_args=raw_args) == should_pass, "Failed to start csync service"
+        if should_pass:
+            assert csync.wait_for_repl_stage(), "Failed to start replication stage"
+    except Exception:
+        raise
+    finally:
+        stop_all_crud_operations()
+        all_threads = []
+        if "operation_threads_1" in locals():
+            all_threads += operation_threads_1
+        if "operation_threads_2" in locals():
+            all_threads += operation_threads_2
+        if "operation_threads_3" in locals():
+            all_threads += operation_threads_3
+        for thread in all_threads:
+            thread.join()
+    if should_pass:
+        assert csync.wait_for_zero_lag(), "Failed to catch up on replication"
+        assert csync.finalize(), "Failed to finalize csync service"
+    assert check_command_output(expected_cmd_return, csync, should_pass)
+    assert expected_log in csync.logs(tail=3000), f"Expected '{expected_log}' does not appear in logs"
+
 @pytest.mark.parametrize("csync_env", [
     {"PCSM_LOG_LEVEL": "DEBUG"},
     {"PCSM_LOG_LEVEL": "INFO"},
@@ -321,3 +369,36 @@ def test_pcsm_log_json_env_var_PML_T76(start_cluster, src_cluster, dst_cluster, 
             raise AssertionError(
                 f"Log line '{line}' is not valid JSON"
             )
+
+@pytest.mark.csync_env({"PCSM_USE_COLLECTION_BULK_WRITE": "True"})
+@pytest.mark.parametrize("cluster_configs", ["replicaset"], indirect=True)
+@pytest.mark.timeout(2700, func_only=True)
+@pytest.mark.parametrize("raw_args, should_pass, expected_log, mode", [
+                            ([], True, "Use collection-level bulk write", "cli"),
+])
+def test_pcsm_use_collection_bulk_write_env_var_PML_T76(start_cluster, src_cluster, dst_cluster, csync, raw_args, expected_log, should_pass, mode, csync_env):
+    """
+    Test the PCSM_USE_COLLECTION_BULK_WRITE environment variable
+    """
+    try:
+        _, operation_threads_1 = create_all_types_db(src_cluster.connection, "init_test_db", start_crud=True, is_sharded=src_cluster.is_sharded)
+        _, operation_threads_2 = create_all_types_db(src_cluster.connection, "clone_test_db", start_crud=True, is_sharded=src_cluster.is_sharded)
+        _, operation_threads_3 = create_all_types_db(src_cluster.connection, "repl_test_db", start_crud=True, is_sharded=src_cluster.is_sharded)
+        assert csync.start(mode=mode, raw_args=raw_args) == should_pass, "Failed to start csync service"
+        assert csync.wait_for_repl_stage(), "Failed to start replication stage"
+    except Exception:
+        raise
+    finally:
+        stop_all_crud_operations()
+        all_threads = []
+        if "operation_threads_1" in locals():
+            all_threads += operation_threads_1
+        if "operation_threads_2" in locals():
+            all_threads += operation_threads_2
+        if "operation_threads_3" in locals():
+            all_threads += operation_threads_3
+        for thread in all_threads:
+            thread.join()
+    assert csync.wait_for_zero_lag(), "Failed to catch up on replication"
+    assert csync.finalize(), "Failed to finalize csync service"
+    assert expected_log in csync.logs(tail=3000), f"Expected '{expected_log}' does not appear in logs"
